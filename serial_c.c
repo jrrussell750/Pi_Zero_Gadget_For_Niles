f9daae0ff2515b37bb61d28c1857fe18db70bd68
jrrussell750 <46250525+jrrussell750@users.noreply.github.com>
2021-05-19 10:13:42 -0500

    Add files via upload

---
+++ serial_c.c
@@ -0,0 +1,90 @@
+#define TERMINAL    "/dev/ttyGS0"
+
+#include <errno.h>
+#include <fcntl.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+int set_interface_attribs(int fd, int speed)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error from tcgetattr: %s\n", strerror(errno));
+        return -1;
+    }
+
+    cfsetospeed(&tty, (speed_t)speed);
+    cfsetispeed(&tty, (speed_t)speed);
+
+    tty.c_cflag |= (CLOCAL | CREAD);    /* ignore modem controls */
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;         /* 8-bit characters */
+    tty.c_cflag &= ~PARENB;     /* no parity bit */
+    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
+    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+
+    /* setup for non-canonical mode */
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    tty.c_oflag &= ~OPOST;
+
+    /* fetch bytes as they become available */
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 1;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        printf("Error from tcsetattr: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+void set_mincount(int fd, int mcount)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error tcgetattr: %s\n", strerror(errno));
+        return;
+    }
+
+    tty.c_cc[VMIN] = mcount ? 1 : 0;
+    tty.c_cc[VTIME] = 5;        /* half second timer */
+
+    if (tcsetattr(fd, TCSANOW, &tty) < 0)
+        printf("Error tcsetattr: %s\n", strerror(errno));
+}
+
+
+int main()
+{
+    char *portname = TERMINAL;
+    int fd;
+    int wlen;
+    char *xstr = "Hello!\n";
+    char *outbuff = "...# Bam IR Capture Unit Ver 0.35      31Oct05..# Firmware Downloadable...";
+    int outbuffsize = 74;
+    int xlen = strlen(xstr);
+
+    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    
+    /*baudrate 38400, 8 bits, no parity, 1 stop bit */
+    set_interface_attribs(fd, B38400);
+    //set_mincount(fd, 0);                /* set to pure timed read */
+
+    /* simple output */
+
+    do {
+        unsigned char buf[80];
+        int rdlen;
+
+        rdlen = read(fd, buf, sizeof(buf) - 1);
+        if (buf[0] == '\x1b')
+            write(fd, outbuff, 74);
+
+       } while (1);
+}
